7b742648609fca82d06f98c8fbae278b125f754c
Jikku80 <jeshan.marz@gmail.com>
2023-03-04 14:48:26 +0545

    new signup

--- public/js/home.js
+++ public/js/home.js
@@ -324,4 +324,224 @@ sendMsg.addEventListener("click", async (e) => {
     };
     msgsiteType.value = "query";
     msgmessage.value = "";
-})
+});
+
+
+(function () {
+    let startbtn = document.querySelector(".newusersignup");
+    let signupform = document.querySelector(".new__signup__sec");
+    let emailsec = document.querySelector(".signup__email__sec");
+    let passsec = document.querySelector(".signup__password__sec");
+    let countrysec = document.querySelector(".signup__country__sec");
+    let namesec = document.querySelector(".signup__name__sec");
+    let toemail = document.querySelector(".toemailsec");
+    let topass = document.querySelector(".topasssec");
+    let tocountry = document.querySelector(".tocountrysec");
+    let signup = document.querySelector("#signmeup");
+    let mname = document.querySelector("#m_name");
+    let memail = document.getElementById("m_email");
+    let mpassword = document.getElementById("m_password");
+    let pwdConfirm = document.getElementById("passwordConfirm");
+    let termbox = document.getElementById("termbox");
+    let country = document.getElementById("country");
+    let backtoemail = document.querySelector(".toemailad");
+    let backtoname = document.querySelector(".toname");
+    let backtopass = document.querySelector(".topassw");
+    let sameE = document.querySelector(".same__email__warn");
+    let same = document.querySelector(".same__user__warn");
+    let whiteSpace = document.querySelector(".white__space");
+    let specialCharac = document.querySelector(".unallowed__charc");
+    let pattern = new RegExp(/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/);
+    // sameE.classList.add("hidden");
+    // same.classList.add("hidden");
+    // whiteSpace.classList.add("hidden");
+    // specialCharac.classList.add("hidden");
+
+    startbtn.addEventListener("click", () => {
+        signupform.classList.remove("hidden");
+    })
+
+
+    toemail.addEventListener("click", () => {
+        if (mname.value < 1) {
+            mname.style.borderColor = "crimson";
+        }
+        else {
+            if (/\s/.test(mname.value)) {
+                whiteSpace.classList.remove("hidden");
+            } else {
+                if (pattern.test(mname.value)) {
+                    specialCharac.classList.remove("hidden");
+                }
+                else {
+                    whiteSpace.classList.add("hidden");
+                    specialCharac.classList.add("hidden");
+                    mname.style.borderColor = "gray";
+                    emailsec.classList.remove("hidden");
+                    namesec.classList.add("hidden");
+                }
+            }
+        }
+
+    })
+    backtoname.addEventListener("click", () => {
+        emailsec.classList.add("hidden");
+        namesec.classList.remove("hidden");
+    })
+
+    topass.addEventListener("click", () => {
+        if (memail.value < 1 || memail.value == "" || memail.value == null) {
+            memail.style.borderColor = "crimson";
+        }
+        else {
+            passsec.classList.remove("hidden");
+            emailsec.classList.add("hidden");
+            memail.style.borderColor = "gray";
+        }
+
+    })
+
+    backtoemail.addEventListener("click", () => {
+        passsec.classList.add("hidden");
+        emailsec.classList.remove("hidden");
+    })
+
+    tocountry.addEventListener("click", () => {
+
+        let mpwdLen = mpassword.value.length;
+        if (mpassword.value < 1 || mpassword.value == "" || mpassword.value == null) {
+            mpassword.style.borderColor = "crimson";
+        }
+        else {
+            if (pwdConfirm.value < 1 || pwdConfirm.value == "" || pwdConfirm.value == null) {
+                pwdConfirm.style.borderColor = "crimson";
+            }
+            else {
+                if (mpwdLen < 9) {
+                    errorAlert("Password must be 9 Characters long")
+                }
+                else {
+                    if (mpassword.value !== pwdConfirm.value) {
+                        errorAlert("Password Does not Match!!! Check Again");
+                    }
+                    else {
+                        countrysec.classList.remove("hidden");
+                        passsec.classList.add("hidden");
+                        mpassword.style.borderColor = "gray";
+                        pwdConfirm.style.borderColor = "gray";
+                    }
+                }
+            }
+        }
+
+    })
+
+    backtopass.addEventListener("click", () => {
+        countrysec.classList.add("hidden");
+        passsec.classList.remove("hidden");
+    })
+
+    signup.addEventListener('click', async (e) => {
+        let mpwdLen = mpassword.value.length;
+
+        if (mname.value < 1 || mname.value == "" || mname.value == null) {
+            return false;
+        }
+        if (/\s/.test(mname.value)) {
+            e.preventDefault();
+            whiteSpace.classList.remove("hidden");
+            return false;
+        }
+        if (pattern.test(mname.value)) {
+            e.preventDefault();
+            specialCharac.classList.remove("hidden");
+            return false;
+        }
+        if (memail.value < 1 || memail.value == "" || memail.value == null) {
+            return false;
+        }
+        if (mpassword.value < 1 || mpassword.value == "" || mpassword.value == null) {
+            return false;
+        }
+        if (pwdConfirm.value < 1 || pwdConfirm.value == "" || pwdConfirm.value == null) {
+            return false;
+        }
+
+        if (mpwdLen < 9) {
+            e.preventDefault();
+            errorAlert("Password must be 9 Characters long")
+            return false;
+        }
+
+        if (mpassword.value !== pwdConfirm.value) {
+            e.preventDefault();
+            errorAlert("Password Does not Match!!! Check Again");
+            return false;
+        }
+
+        if (termbox.checked === false) {
+            e.preventDefault();
+            return false;
+        }
+
+        if (country.value == "0") {
+            e.preventDefault();
+            return false;
+        }
+        e.preventDefault();
+        let load = document.querySelector('.loader');
+        load.classList.remove("hidden")
+        try {
+            const endpoint = '/api/users/signup'
+            await fetch((endpoint), {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: mname.value,
+                    email: memail.value,
+                    password: mpassword.value,
+                    passwordConfirm: pwdConfirm.value,
+                    termsandconditionagreed: termbox.checked,
+                    country: country.value
+                })
+            }).then((response) => {
+                load.classList.add("hidden");
+                if (response.status === 201) {
+                    successAlert("Congrats! You have been hooked ;)");
+                    window.setTimeout(() => {
+                        location.assign('/account/me');
+                    }, 400);
+                }
+                else if (response.status === 409) {
+                    errorAlert("User with this name already exists! Use Different Name!")
+                    same.classList.remove("hidden");
+                }
+                else {
+                    console.log(response)
+                    errorAlert("The Given Email already has a account! Use Forget Password to change your password")
+                    sameE.classList.remove("hidden");
+                }
+            })
+        }
+        catch (err) {
+            console.log(err);
+            errorAlert('Sorry! Something went wrong', err);
+        };
+    })
+
+
+    let termsandcond = document.getElementById("termsandcond");
+    let tac = document.querySelector(".termsandconditions");
+    let termcancel = document.getElementById("termCancel");
+
+    termsandcond.addEventListener("click", () => {
+        tac.classList.remove("hidden");
+    })
+
+    termcancel.addEventListener("click", () => {
+        tac.classList.add('hidden');
+    })
+
+})();
