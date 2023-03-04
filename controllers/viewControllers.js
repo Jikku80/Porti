a4c8e43c6283053bd16c39d02b14482ac2ef891e
Jikku80 <jeshan.marz@gmail.com>
2023-03-04 12:23:13 +0545

    sitemap config

--- controllers/viewControllers.js
+++ controllers/viewControllers.js
@@ -902,17 +902,31 @@ exports.sitemap = catchAsync(async (req, res) => {
 
     let xml_content = [
         '<?xml version="1.0" encoding="UTF-8"?>',
-        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+        `<urlset 
+                xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                    http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">`,
         '  <url>',
         '    <loc>https://www.vporti.com/</loc>',
         '    <lastmod>2023-03-4</lastmod>',
         '    <changefreq>daily</changefreq>',
         '    <priority>1</priority>',
         '  </url>',
+        '  <url>',
+        '    <loc>https://www.vporti.com/account/login</loc>',
+        '    <lastmod>2023-03-4</lastmod>',
+        '    <priority>0.80</priority>',
+        '  </url>',
+        '  <url>',
+        '    <loc>https://www.vporti.com/layouts/highlights</loc>',
+        '    <lastmod>2023-03-4</lastmod>',
+        '    <priority>0.80</priority>',
+        '  </url>',
         '</urlset>'
     ]
     res.set('Content-Type', 'application/xml')
-    res.send(xml_content.join('\n'))
+    res.send(xml_content.join('\n'));
 })
 
 exports.robotxt = catchAsync(async (req, res) => {
